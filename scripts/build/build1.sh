8d4445a23af959a9936d98ce764fb7fabbd28ad7
Craig Rodrigues <rodrigc@FreeBSD.org>
2014-11-23 22:57:18 -0800

    Add a script which does buildworld and buildkernel

---
+++ scripts/build/build1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -x -e
+
+if [ -z "$WORKSPACE" ]; then
+	echo "WORKSPACE environment variable undefined."
+	exit 1
+fi
+
+cat <<EOF
+# Put make.conf entries here
+`echo -e "$MAKE_CONF_ENTRIES"`
+EOF
+) > ${WORKSPACE}/make.conf
+
+set +x
+echo "--------------------------------------------------------------"
+echo ">>> ${WORKSPACE}/make.conf contains:"
+set -x
+cat ${WORKSPACE}/make.conf
+set +x
+echo "--------------------------------------------------------------"
+set -x
+
+make -j 4 buildworld __MAKE_CONF=${WORKSPACE}/make.conf
+make -j 4 buildkernel __MAKE_CONF=${WORKSPACE}/make.conf
+

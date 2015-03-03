f7f5a40e5e6e9dfbd5bc5f1b3a863a27325b8884
Craig Rodrigues <rodrigc@FreeBSD.org>
2015-03-02 20:28:38 -0800

    Add script to test building with external toolchain

---
+++ scripts/build/cross-build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# Copyright (c) 2015, Craig Rodrigues <rodrigc@FreeBSD.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice unmodified, this list of conditions, and the following
+#    disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+set -x -e
+
+if [ -z "$WORKSPACE" ]; then
+	echo "WORKSPACE environment variable undefined."
+	exit 1
+fi
+
+if [ -z "$TARGET_ARCH" ]; then
+	echo "TARGET_ARCH environment variable undefined"
+	exit 1
+fi
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+mkdir -p ${MAKEOBJDIRPREFIX}
+
+(
+cat <<EOF
+# Put make.conf entries here
+`echo -e "$MAKE_CONF_FILE"`
+EOF
+) > ${WORKSPACE}/make.conf
+
+set +x
+echo "--------------------------------------------------------------"
+echo ">>> ${WORKSPACE}/make.conf contains:"
+set -x
+cat ${WORKSPACE}/make.conf
+set +x
+echo "--------------------------------------------------------------"
+set -x
+
+pkg install -y devel/${TARGET_ARCH}-toolchain-gcc
+make -j 4 CROSS_TOOLCHAIN=${TARGET_ARCH}-gcc buildworld __MAKE_CONF=${WORKSPACE}/make.conf
+make -j 4 CROSS_TOOLCHAIN=${TARGET_ARCH}-gcc buildkernel __MAKE_CONF=${TARGET_ARCH}-gcc ${WORKSPACE}/make.conf
+

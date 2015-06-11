77ed7d97856a768ac785c511fb93f7e461dcf81e
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-06-11 13:47:02 +0000

    Add job "test-FreeBSD_Doc-igor"

---
+++ jobs/test-FreeBSD_Doc-igor/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+OUTPUT=checkstyle-result.xml
+
+cd doc
+
+cat << HEADER > ${OUTPUT}
+<?xml version="1.0" encoding="UTF-8"?>
+<checkstyle version="5.0">
+HEADER
+
+igor -clntESW -X `find ./en_US.ISO8859-1/books/handbook -name \*.xml` >> ${OUTPUT}
+
+cat << FOOTER >> ${OUTPUT}
+</checkstyle>
+FOOTER

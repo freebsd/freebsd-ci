6fd5d776cd730fd6f5eb37e2f0c8e2015e2493df
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-06-11 13:57:10 +0000

    Work in the top level of workspace

--- jobs/test-FreeBSD_Doc-igor/build.sh
+++ jobs/test-FreeBSD_Doc-igor/build.sh
@@ -2,14 +2,12 @@
 
 OUTPUT=checkstyle-result.xml
 
-cd doc
-
 cat << HEADER > ${OUTPUT}
 <?xml version="1.0" encoding="UTF-8"?>
 <checkstyle version="5.0">
 HEADER
 
-igor -clntESW -X `find ./en_US.ISO8859-1/books/handbook -name \*.xml` >> ${OUTPUT}
+igor -clntESW -X `find doc/en_US.ISO8859-1/books/handbook -name \*.xml` >> ${OUTPUT}
 
 cat << FOOTER >> ${OUTPUT}
 </checkstyle>

a3b500a781e2a9f0ea04f20a67f53b875b64b3b5
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-07-11 03:38:48 +0800

    Add script to execute job in jail

---
+++ scripts/jail/execute.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+
+JNAME=${JOB_NAME}
+
+echo "--------------------------------------------------------------"
+echo "start build in jail ${JNAME}"
+echo "--------------------------------------------------------------"
+
+sudo jexec ${JNAME} sh -c "cd /workspace && sh -x freebsd-ci/jobs/${JOB_NAME}/build.sh"

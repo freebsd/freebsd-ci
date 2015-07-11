95aa986ecd5cb0e5a475be4dfb53439d46f98d95
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-07-11 22:20:44 +0800

    Test amd64 target

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd src
-make -DNO_CLEAN -j 4 buildworld TARGET=arm64
-make -DNO_CLEAN -j 4 buildkernel TARGET=arm64
+make -DNO_CLEAN -j 4 buildworld TARGET=amd64
+make -DNO_CLEAN -j 4 buildkernel TARGET=amd64

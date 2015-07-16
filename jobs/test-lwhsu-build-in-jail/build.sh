244396cb15c32ef2d7fba612e410c79b52c098b3
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-07-16 14:43:20 +0800

    Test i386 build

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd src
-make -DNO_CLEAN -j 4 buildworld TARGET=amd64
-make -DNO_CLEAN -j 4 buildkernel TARGET=amd64
+make -DNO_CLEAN -j 4 buildworld TARGET=i386
+make -DNO_CLEAN -j 4 buildkernel TARGET=i386

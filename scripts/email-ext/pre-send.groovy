259b5c60e1ec640e5ee35ef8fd402310924f7845
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-08-14 23:14:23 +0800

    Add email-ext pre-send script for checking false-positive patterns then mail to jenkins-admin only

---
+++ scripts/email-ext/pre-send.groovy
@@ -0,0 +1,28 @@
+import javax.mail.*
+import javax.mail.internet.*
+
+Address[] jenkinsAdmin = [
+    'jenkins-admin@FreeBSD.org',
+].collect { new InternetAddress(it) }
+
+patterns = [
+    'hudson.remoting.RequestAbortedException:',
+    'Connection aborted: org.jenkinsci.remoting'
+]
+
+try {
+    logger.write('Checking with false-positive patterns...\n')
+    def logFilePath = build.getLogFile().getPath();
+    String logContent = new File(logFilePath).text;
+    patterns.find {
+        if (logContent.find(/$it/)) {
+            logger.write('******** This is Jenkins internal issue! ********\n')
+            msg.setRecipients(Message.RecipientType.TO, jenkinsAdmin)
+            return true
+        }
+        return false
+    }
+} catch (all) {
+        logger.write('******** Pre-send script got exception! ********\n')
+        msg.setRecipients(Message.RecipientType.TO, jenkinsAdmin)
+}

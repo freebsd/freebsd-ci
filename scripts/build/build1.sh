9fec5b1fb497a132d9bc448e99ce79a90299d4f6
Craig Rodrigues <rodrigc@FreeBSD.org>
2015-12-20 22:46:19 -0800

    Put make.conf in the root of ${WORKSPACE}

--- scripts/build/build1.sh
+++ scripts/build/build1.sh
@@ -36,6 +36,7 @@ fi
 if [ -z "$MAKEOBJDIRPREFIX" ]; then
     export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
 fi
+
 mkdir -p ${MAKEOBJDIRPREFIX}
 
 (
@@ -43,17 +44,17 @@ cat <<EOF
 # Put make.conf entries here
 `echo -e "$MAKE_CONF_FILE"`
 EOF
-) > ${BUILD_ROOT}/make.conf
+) > ${WORKSPACE}/make.conf
 
 set +x
 echo "--------------------------------------------------------------"
-echo ">>> ${BUILD_ROOT}/make.conf contains:"
+echo ">>> ${WORKSPACE}/make.conf contains:"
 set -x
-cat ${BUILD_ROOT}/make.conf
+cat ${WORKSPACE}/make.conf
 set +x
 echo "--------------------------------------------------------------"
 set -x
 
-make -j 4 buildworld __MAKE_CONF=${BUILD_ROOT}/make.conf
-make -j 4 buildkernel __MAKE_CONF=${BUILD_ROOT}/make.conf
+make -j 4 buildworld __MAKE_CONF=${WORKSPACE}/make.conf
+make -j 4 buildkernel __MAKE_CONF=${WORKSPACE}/make.conf
 

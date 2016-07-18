381cac7fe824df47ab138a25a6e9052d5eff2ea9
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-18 16:33:04 +0800

    Use extracted build script

--- jobs/FreeBSD-stable-9-amd64-build/build.sh
+++ jobs/FreeBSD-stable-9-amd64-build/build.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-export MAKEOBJDIRPREFIX=/workspace/obj
-rm -fr ${MAKEOBJDIRPREFIX}
-
-MAKECONF=/dev/null
-SRCCONF=/dev/null
-
-cd src
-
-make -j ${BUILDER_JFLAG} -DNO_CLEAN \
-        buildworld \
-       __MAKE_CONF=${MAKECONF} \
-       SRCCONF=${SRCCONF}
-make -j ${BUILDER_JFLAG} -DNO_CLEAN \
-        buildkernel \
-        __MAKE_CONF=${MAKECONF} \
-        SRCCONF=${SRCCONF}
+env \
+	JFLAG=${BUILDER_JFLAG} \
+	TARGET=amd64 \
+	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

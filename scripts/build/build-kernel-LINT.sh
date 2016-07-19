e5ae312e8de824af7d0feed6d80f796852e1b7db
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-19 18:24:51 +0800

    Add FreeBSD-head-amd64-LINT job

---
+++ scripts/build/build-kernel-LINT.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+WORKSPACE=/workspace
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+
+MAKECONF=/dev/null
+SRCCONF=/dev/null
+
+cd ${WORKSPACE}/src/sys/${TARGET}/conf
+make LINT
+
+cd ${WORKSPACE}/src
+make -j ${JFLAG} -DNO_CLEAN \
+	-DTARGET=${TARGET} \
+        buildkernel \
+	KERNCONF=LINT \
+        __MAKE_CONF=${MAKECONF} \
+        SRCCONF=${SRCCONF}

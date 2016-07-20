843d12ef93811f2301b2f9717dc6ceebf0845bc2
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-20 17:21:06 +0800

    Try to make world and kernel packages

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,7 +1,50 @@
 #!/bin/sh
 
-cd src
-sudo touch aaa
-sudo mkdir -p bbb
-touch ccc
-mkdir -p ddd
+JFLAG=${BUILDER_JFLAG}
+TARGET=amd64
+
+WORKSPACE=/workspace
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+
+MAKECONF=/dev/null
+SRCCONF=/dev/null
+
+DISTDIR=${WORKSPACE}/dist
+
+cd ${WORKSPACE}/src
+
+make -j ${JFLAG} -DNO_CLEAN \
+       buildworld \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF}
+make -j ${JFLAG} -DNO_CLEAN \
+       buildkernel \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF}
+
+sudo make -N -DNO_CLEAN \
+        -DNO_ROOT \
+        distributeworld \
+        __MAKE_CONF=${MAKECONF} \
+        SRCCONF=${SRCCONF} \
+        DISTDIR=${DISTDIR}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+       -DNO_ROOT \
+       packageworld \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF} \
+       DISTDIR=${DISTDIR}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+       -DNO_ROOT \
+       distributekernel \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF} \
+       DISTDIR=${DISTDIR}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+       -DNO_ROOT \
+       packagekernel \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF} \
+       DISTDIR=${DISTDIR}

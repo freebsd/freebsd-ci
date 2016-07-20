09a70e25e800f51cfe1cd1d8f7bf14278f9c409e
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-20 23:36:20 +0800

    Build in /usr/src

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -3,23 +3,16 @@
 JFLAG=${BUILDER_JFLAG}
 TARGET=amd64
 
-WORKSPACE=/workspace
-
-export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
-rm -fr ${MAKEOBJDIRPREFIX}
-
 MAKECONF=/dev/null
 SRCCONF=/dev/null
 
-DISTDIR=${WORKSPACE}/dist
+cd /usr/src
 
-cd ${WORKSPACE}/src
-
-make -j ${JFLAG} -DNO_CLEAN \
+sudo make -j ${JFLAG} -DNO_CLEAN \
        buildworld \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF}
-make -j ${JFLAG} -DNO_CLEAN \
+sudo make -j ${JFLAG} -DNO_CLEAN \
        buildkernel \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF}
@@ -29,22 +22,18 @@ sudo make -DNO_CLEAN \
         distributeworld \
         __MAKE_CONF=${MAKECONF} \
         SRCCONF=${SRCCONF} \
-        DISTDIR=${DISTDIR}
 sudo make -DNO_CLEAN \
        -DNO_ROOT \
        packageworld \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF} \
-       DISTDIR=${DISTDIR}
 sudo make -DNO_CLEAN \
        -DNO_ROOT \
        distributekernel \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF} \
-       DISTDIR=${DISTDIR}
 sudo make -DNO_CLEAN \
        -DNO_ROOT \
        packagekernel \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF} \
-       DISTDIR=${DISTDIR}

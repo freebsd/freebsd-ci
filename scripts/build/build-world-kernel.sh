d9c20bd230593efa7f7e74e1af39d7581e3a9b96
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-23 03:10:56 +0800

    Build world & kernel packages

--- scripts/build/build-world-kernel.sh
+++ scripts/build/build-world-kernel.sh
@@ -8,15 +8,22 @@ rm -fr ${MAKEOBJDIRPREFIX}
 MAKECONF=/dev/null
 SRCCONF=/dev/null
 
-cd ${WORKSPACE}/src
+cd /usr/src
 
-make -j ${JFLAG} -DNO_CLEAN \
-	-DTARGET=${TARGET} \
-        buildworld \
+sudo make -j ${JFLAG} -DNO_CLEAN \
+       buildworld \
+       TARGET=${TARGET} \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF}
-make -j ${JFLAG} -DNO_CLEAN \
-	-DTARGET=${TARGET} \
-        buildkernel \
-        __MAKE_CONF=${MAKECONF} \
-        SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+       buildkernel \
+       TARGET=${TARGET} \
+       __MAKE_CONF=${MAKECONF} \
+       SRCCONF=${SRCCONF}
+
+cd /usr/src/release
+
+sudo make -DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET}
+SRC_REVISION=`svnliteversion /usr/src`
+sudo mkdir -p artifact/${SRC_REVISION}/${TARGET}
+sudo mv ftp/* artifact/${SRC_REVISION}/${TARGET}

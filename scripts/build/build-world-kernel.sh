50ba90f104d5a968fcd7d25b1c67dbb8b4c9bf61
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-23 16:27:04 +0800

    Use TARGET_ARCH

--- scripts/build/build-world-kernel.sh
+++ scripts/build/build-world-kernel.sh
@@ -13,17 +13,19 @@ cd /usr/src
 sudo make -j ${JFLAG} -DNO_CLEAN \
        buildworld \
        TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF}
 sudo make -j ${JFLAG} -DNO_CLEAN \
        buildkernel \
        TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
        __MAKE_CONF=${MAKECONF} \
        SRCCONF=${SRCCONF}
 
 cd /usr/src/release
 
-sudo make -DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET}
+sudo make -DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH}
 SVN_REVISION=`svnliteversion /usr/src`
-sudo mkdir -p artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}
-sudo mv ftp/* artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}
+sudo mkdir -p artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+sudo mv ftp/* artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}/${TARGET_ARCH}

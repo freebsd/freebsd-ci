8973555d534897c7d6db572ccbf8704f23640563
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-07-23 15:54:38 +0800

    Build aarch64/arm64 world&kernel package

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 JFLAG=${BUILDER_JFLAG}
-TARGET=amd64
+TARGET_ARCH=aarch64
+TARGET=arm64
 
 MAKECONF=/dev/null
 SRCCONF=/dev/null
@@ -9,19 +10,21 @@ SRCCONF=/dev/null
 cd /usr/src
 
 sudo make -j ${JFLAG} -DNO_CLEAN \
-       buildworld \
-       TARGET=${TARGET} \
-       __MAKE_CONF=${MAKECONF} \
-       SRCCONF=${SRCCONF}
+	buildworld \
+	TARGET_ARCH=${TARGET_ARCH} \
+	TARGET=${TARGET} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
 sudo make -j ${JFLAG} -DNO_CLEAN \
-       buildkernel \
-       TARGET=${TARGET} \
-       __MAKE_CONF=${MAKECONF} \
-       SRCCONF=${SRCCONF}
+	buildkernel \
+	TARGET_ARCH=${TARGET_ARCH} \
+	TARGET=${TARGET} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
 
 cd /usr/src/release
 
-sudo make -DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET}
+sudo make -DNOPORTS -DNOSRC -DNODOC ftp TARGET_ARCH=${TARGET_ARCH} TARGET=${TARGET}
 SVN_REVISION=`svnliteversion /usr/src`
-sudo mkdir -p artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}
-sudo mv ftp/* artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET}
+sudo mkdir -p artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET_ARCH}/${TARGET}
+sudo mv ftp/* artifact/${FBSD_BRANCH}/${SVN_REVISION}/${TARGET_ARCH}/${TARGET}

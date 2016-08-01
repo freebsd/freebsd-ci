4a612c79a50ccb503d2a81160496da9491618476
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-01 22:55:10 +0800

    Shrink disk size for speed up

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -13,6 +13,6 @@ sudo tar zxvf ../base.txz
 sudo tar zxvf ../kernel.txz
 cd -
 
-sudo makefs -d 6144 -t ffs -f 200000 -s 32g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
+sudo makefs -d 6144 -t ffs -f 200000 -s 2G -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
 mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disc.img
 xz disc.img

6fd40a56480cdd495c3d4cfa9bb7fbd1935a55d5
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-01 17:34:49 +0800

    Compress disk image file

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -15,3 +15,4 @@ cd -
 
 sudo makefs -d 6144 -t ffs -f 200000 -s 32g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
 mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disc.img
+xz disc.img

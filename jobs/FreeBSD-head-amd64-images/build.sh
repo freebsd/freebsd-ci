a3542684ab4b355db83110ae8041e253c2c1e0b0
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-02 17:10:49 +0800

    There is no pushd/popd in sh

--- jobs/FreeBSD-head-amd64-images/build.sh
+++ jobs/FreeBSD-head-amd64-images/build.sh
@@ -14,7 +14,7 @@ ARTIFACT_SUBDIR=${BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
 
 sudo rm -fr work
 mkdir -p work
-pushd work
+cd work
 
 mkdir -p ufs
 for f in base kernel
@@ -27,8 +27,7 @@ sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,l
 mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disc.img
 xz -0 disc.img
 
-popd
-
+cd /workspace
 rm -fr artifact
 mkdir -p artifact/${ARTIFACT_SUBDIR}
 mv work/disc.img.xz artifact/${ARTIFACT_SUBDIR}

3ba4acd6e857ae945c5c3676c38386e4aad02b23
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-03 02:13:03 +0800

    Init test phase

--- jobs/FreeBSD-head-amd64-test/build.sh
+++ jobs/FreeBSD-head-amd64-test/build.sh
@@ -6,28 +6,14 @@ if [ -z "${REVISION}" ]; then
 fi
 
 BRANCH=head
-SVN_REVISION=${REVISION}
 TARGET=amd64
 TARGET_ARCH=amd64
 
 ARTIFACT_SUBDIR=${BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
 
-sudo rm -fr work
-mkdir -p work
-cd work
+fetch http://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/disk-test.img.xz
+xz -d disk-test.img.xz
 
-mkdir -p ufs
-for f in base kernel
-do
-	fetch http://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/${f}.txz
-	sudo tar Jxf ${f}.txz -C ufs
-done
+# run disk-test.img with bhyve
 
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
-mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disk-test.img
-xz -0 disk-test.img
-
-cd /workspace
-rm -fr artifact
-mkdir -p artifact/${ARTIFACT_SUBDIR}
-mv work/disk-test.img.xz artifact/${ARTIFACT_SUBDIR}
+# extract test result

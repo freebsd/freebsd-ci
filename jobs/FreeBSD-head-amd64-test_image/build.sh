3be0e618e3fcad3c374fea07016b8ba29403cee3
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-03 11:52:59 +0800

    Add fstab and resolv.conf

--- jobs/FreeBSD-head-amd64-test_image/build.sh
+++ jobs/FreeBSD-head-amd64-test_image/build.sh
@@ -22,6 +22,20 @@ do
 	sudo tar Jxf ${f}.txz -C ufs
 done
 
+cat > ufs/etc/fstab <<EOF
+# Device        Mountpoint      FStype  Options Dump    Pass#
+/dev/ada0p2     none            swap    sw      0       0
+/dev/ufs/ROOT   /               ufs     rw      1       1
+proc            /proc           procfs  rw      0       0
+EOF
+
+cat > ufs/etc/resolv.conf <<EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 2610:1c1:1:6002::100
+nameserver 2610:1c1:1:6002::200
+EOF
+
 sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
 mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disk-test.img
 xz -0 disk-test.img

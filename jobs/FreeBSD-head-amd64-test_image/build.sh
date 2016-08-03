d0c0e7670f062a495f4e220861599257835d968a
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-03 15:05:02 +0800

    This step might not need sudo

--- jobs/FreeBSD-head-amd64-test_image/build.sh
+++ jobs/FreeBSD-head-amd64-test_image/build.sh
@@ -38,7 +38,7 @@ nameserver 2610:1c1:1:6002::100
 nameserver 2610:1c1:1:6002::200
 EOF
 
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
+makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
 mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disk-test.img
 xz -0 disk-test.img
 

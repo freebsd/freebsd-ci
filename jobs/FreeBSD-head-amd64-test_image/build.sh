78131ef4f206ffa3fb2dee308620797135df237a
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-03 19:05:27 +0800

    Use gpt label

--- jobs/FreeBSD-head-amd64-test_image/build.sh
+++ jobs/FreeBSD-head-amd64-test_image/build.sh
@@ -26,13 +26,17 @@ done
 
 cat <<EOF | sudo tee ufs/etc/fstab
 # Device        Mountpoint      FStype  Options Dump    Pass#
-/dev/ada0p2     none            swap    sw      0       0
-/dev/ufs/ROOT   /               ufs     rw      1       1
-proc            /proc           procfs  rw      0       0
+/dev/gpt/swapfs none            swap    sw      0       0
+/dev/gpt/rootfs /               ufs     rw      1       1
 EOF
 
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
-mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disk-test.img
+sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096 ufs.img ufs
+mkimg -s gpt -f raw  \
+	-b ufs/boot/pmbr \
+	-p freebsd-boot/bootfs:=ufs/boot/gptboot \
+	-p freebsd-swap/swapfs::1G \
+	-p freebsd-ufs/rootfs:=ufs.img \
+	-o disk-test.img
 xz -0 disk-test.img
 
 cd ${WORKSPACE}

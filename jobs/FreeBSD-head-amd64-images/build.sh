c353d50c8f31916d7cffa40f16625d5723566359
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-03 20:24:52 +0800

    Sync chnages in test image

--- jobs/FreeBSD-head-amd64-images/build.sh
+++ jobs/FreeBSD-head-amd64-images/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+WORKSPACE=/workspace
+
 if [ -z "${SVN_REVISION}" ]; then
 	echo "No subversion revision specified"
 	exit 1
@@ -16,17 +18,28 @@ mkdir -p work
 cd work
 
 mkdir -p ufs
-for f in base kernel
+for f in base kernel lib32 base-dbg kernel-dbg lib32-dbg doc tests
 do
 	fetch http://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/${f}.txz
 	sudo tar Jxf ${f}.txz -C ufs
 done
 
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
-mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disk.img
+cat <<EOF | sudo tee ufs/etc/fstab
+# Device        Mountpoint      FStype  Options Dump    Pass#
+/dev/gpt/swapfs none            swap    sw      0       0
+/dev/gpt/rootfs /               ufs     rw      1       1
+EOF
+
+sudo makefs -d 6144 -t ffs -f 200000 -s 16g -o version=2,bsize=32768,fsize=4096 ufs.img ufs
+mkimg -s gpt -f raw \
+	-b ufs/boot/pmbr \
+	-p freebsd-boot/bootfs:=ufs/boot/gptboot \
+	-p freebsd-swap/swapfs::1G \
+	-p freebsd-ufs/rootfs:=ufs.img \
+	-o disk.img
 xz -0 disk.img
 
-cd /workspace
+cd ${WORKSPACE}
 rm -fr artifact
 mkdir -p artifact/${ARTIFACT_SUBDIR}
 mv work/disk.img.xz artifact/${ARTIFACT_SUBDIR}

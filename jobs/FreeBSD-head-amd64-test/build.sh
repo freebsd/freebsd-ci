3b5abcc094f8e03ff20c2aee3672bc088878bc17
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-08 02:06:39 +0800

    Use expect for a valid stdio

--- jobs/FreeBSD-head-amd64-test/build.sh
+++ jobs/FreeBSD-head-amd64-test/build.sh
@@ -19,22 +19,17 @@ xz -fd ${IMG_NAME}.xz
 TEST_VM_NAME=test_vm
 sudo /usr/sbin/bhyvectl --vm=${TEST_VM_NAME} --destroy || true
 sudo /usr/sbin/bhyveload -c stdio -m 2048m -d ${IMG_NAME} ${TEST_VM_NAME}
-sudo /usr/sbin/bhyve -c 2 -m 2048m -A -H -P -g 0 \
+set +e
+expect -c "set timeout 7200; \
+	spawn sudo /usr/sbin/bhyve -c 2 -m 2048m -A -H -P -g 0 \
 	-s 0:0,hostbridge \
 	-s 1:0,lpc \
 	-s 2:0,ahci-hd,${IMG_NAME} \
 	-l com1,stdio \
-	test_vm
-ps auxwww # debug
-BHYVE_PID=`ps auxwww | grep bhyve | grep ${TEST_VM_NAME} | grep -v grep | grep -v sudo | awk '{ print $2 }'`
-while [ 1 ]; do
-	ps ${BHYVE_PID} > /dev/null
-	rc=$?
-	if [ $rc -ne 0 ]; then
-		break
-	fi
-	sleep 1
-done
+	${TEST_VM_NAME}; \
+        expect { eof }"
+rc=$?
+echo "bhyve return code = $rc"
 sudo /usr/sbin/bhyvectl --vm=${TEST_VM_NAME} --destroy
 
 # extract test result
@@ -42,7 +37,7 @@ TMP_DIR=`mktemp -d`
 MD_UNIT=`sudo mdconfig -a -t vnode -f ${IMG_NAME}`
 sudo mount /dev/${MD_UNIT}p3 ${TMP_DIR}
 
-cp ${TMP_DIR}/usr/tests/test-report.* .
+cp ${TMP_DIR}/usr/tests/test-report.* . || true
 
 sudo umount ${TMP_DIR}
 sudo mdconfig -d -u ${MD_UNIT}

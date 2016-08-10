8181b8258d290e08fd367eb490970721497a98e3
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-10 19:10:22 +0800

    Define anuse ${WORKSPACE}

--- jobs/FreeBSD-head-amd64-build/build.sh
+++ jobs/FreeBSD-head-amd64-build/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+WORKSAPCE=/workspace
+
 cd /usr/src
-patch < /workspace/freebsd-ci/jobs/FreeBSD-head-amd64-build/keep-empty-files-in-dist.diff
+patch < ${WORKSAPCE}/freebsd-ci/jobs/FreeBSD-head-amd64-build/keep-empty-files-in-dist.diff
+cd -
 
 env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=amd64 \
 	TARGET_ARCH=amd64 \
-	sh -x freebsd-ci/scripts/build/build-world-kernel.sh
+	sh -x ${WORKSAPCE}/freebsd-ci/scripts/build/build-world-kernel.sh

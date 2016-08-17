dae7e1df45a9a5b85456796e2be72e2097591709
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-18 04:06:04 +0800

    Set WITH_TESTS=1 for 10.x

--- jobs/FreeBSD-stable-10-amd64-build/build.sh
+++ jobs/FreeBSD-stable-10-amd64-build/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+SRCCONF=`dirname $0`/src.conf
 env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=amd64 \
 	TARGET_ARCH=amd64 \
+	SRCCONF=${SRCCONF} \
 	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

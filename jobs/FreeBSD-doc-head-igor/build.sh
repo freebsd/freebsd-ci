458f2409bc31ee0abdf52f53af9d031dd79596aa
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-20 21:23:51 +0800

    Overwrite output file

--- jobs/FreeBSD-doc-head-igor/build.sh
+++ jobs/FreeBSD-doc-head-igor/build.sh
@@ -6,4 +6,4 @@ export SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
 fetch https://people.freebsd.org/~lwhsu/igor/igor
 chmod 755 ./igor
 
-./igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` >> ${OUTPUT}
+./igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` > ${OUTPUT}

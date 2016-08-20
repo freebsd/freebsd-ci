2cf3e24b7c7b9955a01184f9876070db44b8da93
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-08-20 20:58:43 +0800

    Add trusted CA

--- jobs/FreeBSD-doc-head-igor/build.sh
+++ jobs/FreeBSD-doc-head-igor/build.sh
@@ -2,5 +2,7 @@
 
 OUTPUT=checkstyle-result.xml
 
+export SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
 fetch https://people.freebsd.org/~lwhsu/igor/igor
+
 ./igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` >> ${OUTPUT}

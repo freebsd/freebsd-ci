43ea9c282895245d6b3cd70337fe23c2b4e99f3c
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-01 15:22:38 +0800

    Add phabric test

---
+++ jobs/FreeBSD-doc-head-igor-phabric/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+OUTPUT=checkstyle-result.xml
+
+export SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
+fetch https://people.freebsd.org/~lwhsu/igor/igor
+chmod 755 ./igor
+
+echo DIFF_ID=${DIFF_ID}
+echo PHID=${PHID}
+
+./igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` > ${OUTPUT}

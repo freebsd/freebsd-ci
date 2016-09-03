b6d3708d9b604d80f61681619a3436defe3fea87
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-03 22:49:24 +0800

    Test quarantine mode

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-sudo rm -fr work
-mkdir -p work
-cd work
+sudo ls -al /var/audit
 
-fetch http://artifact.ci.freebsd.org/snapshot/head/r303605/amd64/amd64/base.txz
-fetch http://artifact.ci.freebsd.org/snapshot/head/r303605/amd64/amd64/kernel.txz
+fetch https://people.freebsd.org/~lwhsu/FreeBSD_logo.png
 
-mkdir -p ufs
-cd ufs
-sudo tar zxvf ../base.txz
-sudo tar zxvf ../kernel.txz
-cd -
-
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096,label=ROOT ufs.img ufs
-mkimg -s gpt -b ufs/boot/pmbr -p freebsd-boot:=ufs/boot/gptboot -p freebsd-swap::1G -p freebsd-ufs:=ufs.img -o disc-r303605.img
-xz -0 disc-r303605.img
+ls -al /tmp

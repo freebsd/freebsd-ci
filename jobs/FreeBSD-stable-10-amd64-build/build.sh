54939fee6b4b620920fc2614b7ce3aab8bb15de7
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-10 18:45:40 +0000

    Also use make.conf

--- jobs/FreeBSD-stable-10-amd64-build/build.sh
+++ jobs/FreeBSD-stable-10-amd64-build/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 SRCCONF=/workspace/`dirname $0`/src.conf
+MAKECONF=/workspace/`dirname $0`/make.conf
 
 sudo cp ${SRCCONF} /etc
+sudo cp ${MAKECONF} /etc
 env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=amd64 \
 	TARGET_ARCH=amd64 \
 	SRCCONF=${SRCCONF} \
+	MAKECONF=${MAKECONF} \
 	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

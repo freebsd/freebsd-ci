91fcb794904b1de4d06a1cd48d46dcd9fd3f2f4f
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-12 18:34:52 +0000

    Try to pass SRCCONF when building under src/release

--- jobs/FreeBSD-stable-10-amd64-build/build.sh
+++ jobs/FreeBSD-stable-10-amd64-build/build.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
 
-SRCCONF=/workspace/`dirname $0`/src.conf
-MAKECONF=/workspace/`dirname $0`/make.conf
+JFLAG=${BUILDER_JFLAG}
 
-sudo cp ${SRCCONF} /etc
-sudo cp ${MAKECONF} /etc
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=amd64 \
-	TARGET_ARCH=amd64 \
-	SRCCONF=${SRCCONF} \
-	MAKECONF=${MAKECONF} \
-	sh -x freebsd-ci/scripts/build/build-world-kernel.sh
+TARGET=amd64
+TARGET_ARCH=amd64
+
+SRCCONF=/workspace/`dirname $0`/src.conf
+MAKECONF=/dev/null
+
+WORKSPACE=/workspace
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+
+cd /usr/src
+
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	buildworld \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo make -j ${JFLAG} -DNO_CLEAN \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+
+cd /usr/src/release
+
+sudo make MAKE="make __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}" \
+	-DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH}
+
+ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+sudo mkdir -p ${ARTIFACT_DEST}
+sudo mv ftp/* ${ARTIFACT_DEST}
+
+echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

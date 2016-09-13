17d9500451588610b689be5b67dd78424584bc8b
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-13 20:33:46 +0800

    Make general build script support __MAKE_CONF and SRCCONF

--- jobs/FreeBSD-stable-10-amd64-build/build.sh
+++ jobs/FreeBSD-stable-10-amd64-build/build.sh
@@ -1,40 +1,12 @@
 #!/bin/sh
 
-JFLAG=${BUILDER_JFLAG}
-
-TARGET=amd64
-TARGET_ARCH=amd64
-
 SRCCONF=/workspace/`dirname $0`/src.conf
-MAKECONF=/dev/null
 
 WORKSPACE=/workspace
 
-export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
-rm -fr ${MAKEOBJDIRPREFIX}
-
-cd /usr/src
-
-sudo make -j ${JFLAG} -DNO_CLEAN \
-	buildworld \
-	TARGET=${TARGET} \
-	TARGET_ARCH=${TARGET_ARCH} \
-	__MAKE_CONF=${MAKECONF} \
-	SRCCONF=${SRCCONF}
-sudo make -j ${JFLAG} -DNO_CLEAN \
-	buildkernel \
-	TARGET=${TARGET} \
-	TARGET_ARCH=${TARGET_ARCH} \
-	__MAKE_CONF=${MAKECONF} \
-	SRCCONF=${SRCCONF}
-
-cd /usr/src/release
-
-sudo make MAKE="make __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}" \
-	-DNOPORTS -DNOSRC -DNODOC ftp TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH}
-
-ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
-sudo mkdir -p ${ARTIFACT_DEST}
-sudo mv ftp/* ${ARTIFACT_DEST}
-
-echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property
+env \
+	JFLAG=${BUILDER_JFLAG} \
+	SRCCONF=${SRCCONF} \
+	TARGET=amd64 \
+	TARGET_ARCH=amd64 \
+	sh -x ${WORKSAPCE}/freebsd-ci/scripts/build/build-world-kernel.sh

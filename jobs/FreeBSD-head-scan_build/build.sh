57f95deecd6c3026cae8d4c6159000bf898846e6
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-14 19:17:18 +0000

    Swtich to src location

--- jobs/FreeBSD-head-scan_build/build.sh
+++ jobs/FreeBSD-head-scan_build/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-set +e
+cd src
 
+set +e
 for d in bin sbin usr.bin usr.sbin lib libexec sys; do
 	cd ${d};
 	make -j ${BUILDER_JFLAG} CLANG_ANALYZE_OUTPUT_DIR=${WORKSPACE}/clangScanBuildReports CLANG_ANALYZE_OUTPUT=html analyze

9418b5ab7e0f38eacdb9e39c66cc777fa2c320a5
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-14 19:23:36 +0000

    Set MAKEOBJDIRPREFIX and use make -i

--- jobs/FreeBSD-head-scan_build/build.sh
+++ jobs/FreeBSD-head-scan_build/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+
 cd src
 
-set +e
 for d in bin sbin usr.bin usr.sbin lib libexec sys; do
 	cd ${d};
-	make -j ${BUILDER_JFLAG} CLANG_ANALYZE_OUTPUT_DIR=${WORKSPACE}/clangScanBuildReports CLANG_ANALYZE_OUTPUT=html analyze
+	make -i -j ${BUILDER_JFLAG} CLANG_ANALYZE_OUTPUT_DIR=${WORKSPACE}/clangScanBuildReports CLANG_ANALYZE_OUTPUT=html analyze
 	cd -
 done

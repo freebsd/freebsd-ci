8b5711be58504fd217c4ce5f38d767568fbbd490
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-15 19:52:04 +0000

    Use KEEP_WORKSPACE_PATH=1

--- jobs/FreeBSD-head-scan_build/build.sh
+++ jobs/FreeBSD-head-scan_build/build.sh
@@ -3,17 +3,13 @@
 SRCCONF=/workspace/`dirname $0`/src.conf
 MAKECONF=/workspace/`dirname $0`/make.conf
 
-WORKSPACE=/workspace
-
 export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
 rm -fr ${MAKEOBJDIRPREFIX}
 
 CLANG_ANALYZE_OUTPUT_DIR=${WORKSPACE}/clangScanBuildReports
 rm -fr ${CLANG_ANALYZE_OUTPUT_DIR}
 
-mkdir -p ${JOB_NAME}
-ln -sf ../src ${JOB_NAME}/src
-cd ${JOB_NAME}/src
+cd ${WORKSPACE}/src
 
 set -e
 #for d in bin sbin usr.bin usr.sbin lib libexec sys; do

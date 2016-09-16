ad5c35d115359458b7a4fe027948d8a3d2ebcf49
Li-Wen Hsu <lwhsu@lwhsu.org>
2016-09-16 18:17:08 +0000

    igor-1.595 has official package now

--- jobs/FreeBSD-doc-head-igor/build.sh
+++ jobs/FreeBSD-doc-head-igor/build.sh
@@ -2,8 +2,4 @@
 
 OUTPUT=checkstyle-result.xml
 
-export SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
-fetch https://people.freebsd.org/~lwhsu/igor/igor
-chmod 755 ./igor
-
-./igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` > ${OUTPUT}
+igor -clntESW -X `find doc/en_US.ISO8859-1 -name \*.xml` > ${OUTPUT}

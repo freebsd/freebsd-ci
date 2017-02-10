c2c786fcb1288bac2bf538f46f45f417c60f2c35
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-10 16:21:04 +0800

    Really set WITHOUT_FORMAT_EXTENSIONS=yes

--- jobs/FreeBSD-head-riscv64-build/build.sh
+++ jobs/FreeBSD-head-riscv64-build/build.sh
@@ -4,6 +4,5 @@ env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=riscv \
 	TARGET_ARCH=riscv64 \
-	MAKE_ARGS=CROSS_TOOLCHAIN=riscv64-gcc \
-	WITHOUT_FORMAT_EXTENSIONS=yes \
+	MAKE_ARGS="CROSS_TOOLCHAIN=riscv64-gcc WITHOUT_FORMAT_EXTENSIONS=yes" \
 	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

4e43e5e4ff1152f11957e3d99ef7523d21c27296
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-11 00:35:44 +0800

    Correct scripts for RISC-V of 11-STABLE

--- jobs/FreeBSD-stable-11-riscv64-build/build.sh
+++ jobs/FreeBSD-stable-11-riscv64-build/build.sh
@@ -1,8 +1,69 @@
 #!/bin/sh
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=riscv \
-	TARGET_ARCH=riscv64 \
-	MAKE_ARGS="CROSS_TOOLCHAIN=riscv64-gcc WITHOUT_FORMAT_EXTENSIONS=yes" \
-	sh -x freebsd-ci/scripts/build/build-world-kernel.sh
+IMAGE_NAME=riscv.img
+JOB_BASE=${WORKSPACE}/freebsd-ci/jobs/${JOB_NAME}
+
+TARGET=riscv
+TARGET_ARCH=riscv64
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+export DESTDIR=${WORKSPACE}/dest
+rm -fr ${DESTDIR}
+
+MAKECONF=${MAKECONF:-/dev/null}
+SRCCONF=${SRCCONF:-/dev/null}
+
+cd ${WORKSPACE}/src
+
+make -j ${BUILDER_JFLAG} \
+	-DNO_CLEAN \
+	CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	buildworld
+
+make CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	-DNO_CLEAN \
+	-DNO_ROOT \
+	-DWITHOUT_TESTS \
+	DESTDIR=${DESTDIR} \
+	installworld
+make CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	-DNO_CLEAN \
+	-DNO_ROOT \
+	-DWITHOUT_TESTS \
+	DESTDIR=${DESTDIR} \
+	distribution
+
+cd ${WORKSPACE}
+src/tools/tools/makeroot/makeroot.sh -s 32m -f ${JOB_BASE}/basic.files ${IMAGE_NAME} ${DESTDIR}
+
+cat ${JOB_BASE}/QEMUTEST | sed -e "s,%%MFS_IMAGE%%,${WORKSPACE}/${IMAGE_NAME}," | tee src/sys/riscv/conf/QEMUTEST
+
+cd ${WORKSPACE}/src
+make -j ${BUILDER_JFLAG} \
+	-DNO_CLEAN \
+	CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	KERNCONF=QEMUTEST \
+	MODULES_OVERRIDE='' \
+	WITHOUT_FORMAT_EXTENSIONS=yes \
+	buildkernel
+
+cd ${WORKSPACE}
+ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+KERNEL_FILE=${MAKEOBJDIRPREFIX}/${TARGET}.${TARGET_ARCH}${WORKSPACE}/src/sys/QEMUTEST/kernel
+rm -fr ${ARTIFACT_DEST}
+mkdir -p ${ARTIFACT_DEST}
+xz ${IMAGE_NAME}
+cp ${IMAGE_NAME}.xz ${ARTIFACT_DEST}
+xz ${KERNEL_FILE}
+cp ${KERNEL_FILE}.xz ${ARTIFACT_DEST}
+
+echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

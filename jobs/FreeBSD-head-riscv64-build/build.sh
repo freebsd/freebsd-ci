e17c1d215403219bb05742833510fe3ac20a540b
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-10 21:47:42 +0800

    RISC-V needs special script

--- jobs/FreeBSD-head-riscv64-build/build.sh
+++ jobs/FreeBSD-head-riscv64-build/build.sh
@@ -1,8 +1,59 @@
 #!/bin/sh
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
+IMAGE_NAME=riscv.img
+
+export MAKEOBJDIRPREFIX=${WORKSPACE}/obj
+rm -fr ${MAKEOBJDIRPREFIX}
+export DESTDIR=${WORKSPACE}/dest
+rm -fr ${DESTDIR}
+
+MAKECONF=${MAKECONF:-/dev/null}
+SRCCONF=${SRCCONF:-/dev/null}
+
+cd ${WORKSPACE}/src
+
+make -j ${BUILDER_JFLAG} \
+	-DNO_CLEAN \
+	CROSS_TOOLCHAIN=riscv64-gcc
 	TARGET=riscv \
 	TARGET_ARCH=riscv64 \
-	MAKE_ARGS="CROSS_TOOLCHAIN=riscv64-gcc WITHOUT_FORMAT_EXTENSIONS=yes" \
-	sh -x freebsd-ci/scripts/build/build-world-kernel.sh
+	WITHOUT_FORMAT_EXTENSIONS=yes \
+	buildworld
+
+make CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=riscv \
+	TARGET_ARCH=riscv64 \
+	-DNO_CLEAN \
+	-DNO_ROOT \
+	-DWITHOUT_TESTS \
+	DESTDIR=${DESTDIR} \
+	installworld
+make CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=riscv \
+	TARGET_ARCH=riscv64 \
+	-DNO_CLEAN \
+	-DNO_ROOT \
+	-DWITHOUT_TESTS \
+	DESTDIR=${DESTDIR} \
+	distribution
+
+cd ${WORKSPACE}
+src/tools/tools/makeroot/makeroot.sh -s 32m -f basic.files ${IMAGE_NAME} ${DESTDIR}
+
+cat QEMUTEST | sed -e "s,%%MFS_IMAGE%%,${WORKSPACE}/${IMAGE_NAME}," | tee src/sys/riscv/conf/QEMUTEST
+
+make -j ${BUILDER_JFLAG} \
+	-DNO_CLEAN \
+	CROSS_TOOLCHAIN=riscv64-gcc \
+	TARGET=riscv \
+	TARGET_ARCH=riscv64 \
+	KERNCONF=QEMUTEST \
+	MODULES_OVERRIDE='' \
+	buildkernel
+
+ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+mkdir -p ${ARTIFACT_DEST}
+cp ${IMAGE_NAME} ${ARTIFACT_DEST}
+cp ${MAKEOBJDIRPREFIX}/${TARGET}.${TARGET_ARCH}${WORKSPACE}/src/sys/QEMUTEST/kernel ${ARTIFACT_DEST}
+
+echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

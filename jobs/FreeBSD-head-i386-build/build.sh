b535cf852a7bee656982c7899710822e0494d385
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-13 02:46:32 +0800

    Yet another

--- jobs/FreeBSD-head-i386-build/build.sh
+++ jobs/FreeBSD-head-i386-build/build.sh
@@ -4,5 +4,5 @@ env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=i386 \
 	TARGET_ARCH=i386 \
-	MAKE_ARGS="XZ_CMD='xz -T 8'" \
+	MAKE_ARGS='XZ_CMD="xz -T 8"' \
 	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

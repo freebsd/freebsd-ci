cfc7f145774264c003f5e526f1f0a452fc52939d
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-14 02:40:52 +0800

    Set $HOME when jevec -U jenkins

--- scripts/jail/execute.sh
+++ scripts/jail/execute.sh
@@ -8,4 +8,4 @@ echo "--------------------------------------------------------------"
 echo "start build in jail ${JNAME}"
 echo "--------------------------------------------------------------"
 
-sudo jexec -U jenkins ${JNAME} sh -c "cd ${WORKSPACE_IN_JAIL} && env WORKSPACE=${WORKSPACE_IN_JAIL} sh -ex freebsd-ci/jobs/${JOB_NAME}/build.sh"
+sudo jexec -U jenkins ${JNAME} sh -c "cd ${WORKSPACE_IN_JAIL} && env HOME=${WORKSPACE_IN_JAIL} WORKSPACE=${WORKSPACE_IN_JAIL} sh -ex freebsd-ci/jobs/${JOB_NAME}/build.sh"

7ae8676fb5523d379d11d099bbd3e76544f5dda0
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-13 11:08:49 +0800

    Use shared script

--- jobs/FreeBSD-head-i386-testvm/build.sh
+++ jobs/FreeBSD-head-i386-testvm/build.sh
@@ -1,69 +1,11 @@
 #!/bin/sh
 
-SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
-
-if [ -z "${SVN_REVISION}" ]; then
-	echo "No subversion revision specified"
-	exit 1
-fi
-
-BRANCH=head
-TARGET=i386
-TARGET_ARCH=i386
-
-ARTIFACT_SUBDIR=${BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
-OUTPUT_IMG_NAME=disk-test.img
-
-sudo rm -fr work
-mkdir -p work
-cd work
-
-mkdir -p ufs
-for f in base kernel base-dbg kernel-dbg doc tests
-do
-	fetch https://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/${f}.txz
-	sudo tar Jxf ${f}.txz -C ufs
-done
-
-sudo cp /etc/resolv.conf ufs/etc/
-sudo chroot ufs env ASSUME_ALWAYS_YES=yes pkg update
-sudo chroot ufs pkg install -y kyua
-
-cat <<EOF | sudo tee ufs/etc/fstab
-# Device        Mountpoint      FStype  Options Dump    Pass#
-/dev/gpt/swapfs none            swap    sw      0       0
-/dev/gpt/rootfs /               ufs     rw      1       1
-EOF
-
-cat <<EOF | sudo tee ufs/etc/rc.local
-#!/bin/sh -ex
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-export PATH
-echo
-echo "--------------------------------------------------------------"
-echo "start kyua tests!"
-echo "--------------------------------------------------------------"
-cd /usr/tests
-/usr/local/bin/kyua test
-/usr/local/bin/kyua report --verbose --results-filter passed,skipped,xfail,broken,failed --output test-report.txt
-/usr/local/bin/kyua report-junit --output=test-report.xml
-shutdown -p now
-EOF
-
-sudo rm -f ufs/etc/resolv.conf
-
-sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096 ufs.img ufs
-mkimg -s gpt -f raw \
-	-b ufs/boot/pmbr \
-	-p freebsd-boot/bootfs:=ufs/boot/gptboot \
-	-p freebsd-swap/swapfs::1G \
-	-p freebsd-ufs/rootfs:=ufs.img \
-        -o ${OUTPUT_IMG_NAME}
-xz -0 ${OUTPUT_IMG_NAME}
-
-cd ${WORKSPACE}
-rm -fr artifact
-mkdir -p artifact/${ARTIFACT_SUBDIR}
-mv work/${OUTPUT_IMG_NAME}.xz artifact/${ARTIFACT_SUBDIR}
-
-echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property
+env \
+	JFLAG=${BUILDER_JFLAG} \
+	TARGET=i386 \
+	TARGET_ARCH=i386 \
+	WITH_LIB32=0 \
+	WITH_DEBUG=1 \
+	WITH_DOC=1 \
+	WITH_TESTS=1 \
+	sh -x freebsd-ci/scripts/build/build-test_image.sh

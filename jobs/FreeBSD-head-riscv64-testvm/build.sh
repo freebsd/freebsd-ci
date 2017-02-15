212b74fd4545406f420604787257f2b9ec10b469
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-02-15 22:47:13 +0800

    build bbl-spike

--- jobs/FreeBSD-head-riscv64-testvm/build.sh
+++ jobs/FreeBSD-head-riscv64-testvm/build.sh
@@ -11,29 +11,48 @@ if [ -z "${SVN_REVISION}" ]; then
 fi
 
 ARTIFACT_SUBDIR=${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
-
-rm -f kernel.xz kernel
-fetch https://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/kernel.xz
-xz -d kernel.xz
+ARTIFACT_DEST=${WORKSPACE}/artifact/${ARTIFACT_SUBDIR}
+mkdir -p ${ARTIFACT_DEST}
 
 rm -fr riscv-pk
 git config --global http.proxy ${BUILDER_HTTP_PROXY}
 git clone --depth=1 --single-branch https://github.com/freebsd-riscv/riscv-pk
-
-mkdir riscv-pk/build/
-cd riscv-pk/build/
+cd riscv-pk
 git rev-parse HEAD
 
+cd ${WORKSPACE}
+rm -f kernel-qemu.xz kernel-qemu
+fetch https://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/kernel-qemu.xz
+xz -d kernel-qemu.xz
+
+rm -fr riscv-pk/build/
+mkdir riscv-pk/build/
+cd riscv-pk/build/
+
 export CFLAGS="-nostdlib"
-../configure --host=riscv64-unknown-freebsd11.0 --with-payload=${WORKSPACE}/kernel
+../configure --host=riscv64-unknown-freebsd11.0 --with-payload=${WORKSPACE}/kernel-qemu
 gmake LIBS=''
 
 mv bbl bbl-qemu
 xz bbl-qemu
+mv bbl-qemu.xz ${ARTIFACT_DEST}
 
 cd ${WORKSPACE}
-ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
-mkdir -p ${ARTIFACT_DEST}
-mv riscv-pk/build/bbl-qemu.xz ${ARTIFACT_DEST}
+rm -f kernel-spike.xz kernel-spike
+fetch https://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/kernel-spike.xz
+xz -d kernel-spike.xz
 
+rm -fr riscv-pk/build/
+mkdir riscv-pk/build/
+cd riscv-pk/build/
+
+export CFLAGS="-nostdlib"
+../configure --host=riscv64-unknown-freebsd11.0 --with-payload=${WORKSPACE}/kernel-spike
+gmake LIBS=''
+
+mv bbl bbl-spike
+xz bbl-spike
+mv bbl-spike.xz ${ARTIFACT_DEST}
+
+cd ${WORKSPACE}
 echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

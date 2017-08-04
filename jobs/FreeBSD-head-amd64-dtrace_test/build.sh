c4d90dd1f1472b9ce94e68ceb823d24f45f4fa5b
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-08-04 08:12:43 +0800

    And need to specify timeout for expect, or default is 10s

--- jobs/FreeBSD-head-amd64-dtrace_test/build.sh
+++ jobs/FreeBSD-head-amd64-dtrace_test/build.sh
@@ -21,7 +21,8 @@ TEST_VM_NAME=test_vm_${EXECUTOR_NUMBER}
 sudo /usr/sbin/bhyvectl --vm=${TEST_VM_NAME} --destroy || true
 sudo /usr/sbin/bhyveload -c stdio -m 4096m -d ${IMG_NAME} ${TEST_VM_NAME}
 set +e
-expect -c "spawn sudo /usr/bin/timeout -k 60 3420 /usr/sbin/bhyve \
+expect -c "set timeout 3540; \
+	spawn sudo /usr/bin/timeout -k 60 3420 /usr/sbin/bhyve \
 	-c 2 -m 4096m -A -H -P -g 0 \
 	-s 0:0,hostbridge \
 	-s 1:0,lpc \

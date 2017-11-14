cff6dc679c2efb60ba9b80c8f7a21814bf04c107
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-11-15 01:17:54 +0800

    Clean before making release

--- scripts/build/build-world-kernel.sh
+++ scripts/build/build-world-kernel.sh
@@ -25,6 +25,7 @@ sudo make -j ${JFLAG} -DNO_CLEAN \
 
 cd /usr/src/release
 
+sudo make clean
 sudo make -DNOPORTS -DNOSRC -DNODOC ftp \
 	TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
 	MAKE="make __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}"

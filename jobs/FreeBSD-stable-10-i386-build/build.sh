5ae19138896239732608bd0b2f98333a3ad4f36b
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-01-24 18:17:24 +0800

    Set WITH_TESTS=yes for i386 10-STABLE

--- jobs/FreeBSD-stable-10-i386-build/build.sh
+++ jobs/FreeBSD-stable-10-i386-build/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+SRCCONF=${WORKSPACE}/`dirname $0`/src.conf
+
 env \
 	JFLAG=${BUILDER_JFLAG} \
+	SRCCONF=${SRCCONF} \
 	TARGET=i386 \
 	TARGET_ARCH=i386 \
 	sh -x freebsd-ci/scripts/build/build-world-kernel.sh

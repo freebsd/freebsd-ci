61bc6a42076ee2897f0170e5ee4143848d428121
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-01-30 02:22:37 +0800

    target is enough

--- scripts/build/build-world-kernel-head.sh
+++ scripts/build/build-world-kernel-head.sh
@@ -26,12 +26,13 @@ sudo make -j ${JFLAG} -DNO_CLEAN \
 cd /usr/src/release
 
 sudo make clean
-sudo make -DNOPORTS -DNOSRC -DNODOC ftp \
+sudo make -DNOPORTS -DNOSRC -DNODOC packagesystem \
 	TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
 	MAKE="make __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}"
 
 ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
 sudo mkdir -p ${ARTIFACT_DEST}
-sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/ftp/* ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/*.txz ${ARTIFACT_DEST}
+sudo mv /usr/obj/usr/src/${TARGET}.${TARGET_ARCH}/release/MANIFEST ${ARTIFACT_DEST}
 
 echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property
--- scripts/build/build-world-kernel.sh
+++ scripts/build/build-world-kernel.sh
@@ -26,12 +26,12 @@ sudo make -j ${JFLAG} -DNO_CLEAN \
 cd /usr/src/release
 
 sudo make clean
-sudo make -DNOPORTS -DNOSRC -DNODOC ftp \
+sudo make -DNOPORTS -DNOSRC -DNODOC packagesystem \
 	TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
 	MAKE="make __MAKE_CONF=${MAKECONF} SRCCONF=${SRCCONF}"
 
 ARTIFACT_DEST=artifact/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
 sudo mkdir -p ${ARTIFACT_DEST}
-sudo mv ftp/* ${ARTIFACT_DEST}
+sudo mv *.txz MANIFEST ${ARTIFACT_DEST}
 
 echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

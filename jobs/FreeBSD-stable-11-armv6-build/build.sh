002f775e745ad58570b4b8bfa6cd61c7e6318a71
Kyle Evans <kevans@FreeBSD.org>
2018-02-05 08:16:10 -0600

    Add src.conf and KERNCONF's to be used for stable-11-armv6; GENERIC is not available there

--- jobs/FreeBSD-stable-11-armv6-build/build.sh
+++ jobs/FreeBSD-stable-11-armv6-build/build.sh
@@ -4,4 +4,5 @@ env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=arm \
 	TARGET_ARCH=armv6 \
+	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
 	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel.sh

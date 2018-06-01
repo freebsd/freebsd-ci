de30597b81684aada63167f3105e4aadea55513b
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-06-01 06:44:36 +0000

    Add releng/11.2 jobs
    Sponsored by:   The FreeBSD Foundation

---
+++ jobs/FreeBSD-releng-11_2-amd64-images/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+env \
+	JFLAG=${BUILDER_JFLAG} \
+	TARGET=amd64 \
+	TARGET_ARCH=amd64 \
+	WITH_LIB32=1 \
+	WITH_DEBUG=1 \
+	WITH_DOC=1 \
+	WITH_TESTS=1 \
+	sh -x freebsd-ci/scripts/build/build-images.sh

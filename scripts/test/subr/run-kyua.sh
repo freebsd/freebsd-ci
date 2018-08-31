b7ce85e700abd12079922b1a05a32af89c3559e9
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-08-31 04:38:47 +0100

    kyua will return 1 when any of the executed test cases fails
    Sponsored by:	The FreeBSD Foundation

--- scripts/test/subr/run-kyua.sh
+++ scripts/test/subr/run-kyua.sh
@@ -6,7 +6,13 @@ echo "start kyua tests!"
 echo "--------------------------------------------------------------"
 
 cd /usr/tests
+set +e
 /usr/local/bin/kyua test                                                       
+rc=$?
+set -e
+if [ ${rc} -ne 0 ] && [ ${rc} -ne 1 ]; then
+	exit ${rc}
+fi
 /usr/local/bin/kyua report --verbose --results-filter passed,skipped,xfail,broken,failed --output test-report.txt
 /usr/local/bin/kyua report-junit --output=test-report.xml                      
 mv test-report.* /tmp/meta

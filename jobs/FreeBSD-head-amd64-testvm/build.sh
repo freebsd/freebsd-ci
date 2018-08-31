e91d730ea7f7be4188229f67707643abc8c4080e
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-08-31 18:44:33 +0100

    Use export instead of long list passing to env(1)
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-amd64-testvm/build.sh
+++ jobs/FreeBSD-head-amd64-testvm/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=amd64 \
-	TARGET_ARCH=amd64 \
-	WITH_LIB32=1 \
-	WITH_DEBUG=1 \
-	WITH_DOC=1 \
-	WITH_TESTS=1 \
-	sh -x freebsd-ci/scripts/build/build-test_image.sh
+export JFLAG=${BUILDER_JFLAG}
+
+export TARGET=amd64
+export TARGET_ARCH=amd64
+
+export WITH_LIB32=1
+export WITH_DEBUG=1
+export WITH_DOC=1
+export WITH_TESTS=1
+
+sh -x freebsd-ci/scripts/build/build-test_image.sh

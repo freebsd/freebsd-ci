3b01a09d36fe7ce2f1ff22525df24b4fc75f5244
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-09-03 23:41:20 +0100

    Unmount the temporarily /tmp
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-amd64-test_zfs/meta/run.sh
+++ jobs/FreeBSD-head-amd64-test_zfs/meta/run.sh
@@ -22,6 +22,8 @@ if [ ${rc} -ne 0 ] && [ ${rc} -ne 1 ]; then
 	exit ${rc}
 fi
 
+umount /tmp
+
 /usr/local/bin/kyua report --verbose --results-filter passed,skipped,xfail,broken,failed --output test-report.txt
 /usr/local/bin/kyua report-junit --output=test-report.xml
 mv test-report.* ${METADIR}

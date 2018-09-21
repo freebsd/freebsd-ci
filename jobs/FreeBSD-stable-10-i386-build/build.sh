efa9d4a2edfb3c4b8d553022d0b532c0a831af6c
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-09-22 00:45:32 +0300

    Limit xz thread count when cross build i386 on amd64 for 10
    Also rise the thread count from 8 to 10 for head and 11
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-stable-10-i386-build/build.sh
+++ jobs/FreeBSD-stable-10-i386-build/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 SRCCONF=${WORKSPACE}/`dirname $0`/src.conf
+MAKECONF=${WORKSPACE}/`dirname $0`/make.conf
 
 env \
 	JFLAG=${BUILDER_JFLAG} \
+	MAKECONF=${MAKECONF} \
 	SRCCONF=${SRCCONF} \
 	TARGET=i386 \
 	TARGET_ARCH=i386 \

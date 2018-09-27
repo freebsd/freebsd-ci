a51c2cbd242ed98380e94b8676ddea5252005caa
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-09-27 13:27:27 +0200

    Add testvm and test jobs for openssl111
    Sponsored by:	The FreeBSD Foundation

---
+++ jobs/FreeBSD-srcproj-openssl111-testvm/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export JFLAG=${BUILDER_JFLAG}
+
+export TARGET=amd64
+export TARGET_ARCH=amd64
+
+export WITH_LIB32=1
+export WITH_DEBUG=1
+export WITH_DOC=1
+export WITH_TESTS=1
+
+sh -x freebsd-ci/scripts/build/build-test_image.sh

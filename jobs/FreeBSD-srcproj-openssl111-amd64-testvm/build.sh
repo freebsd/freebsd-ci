384c4dbea5c3ed42bd629e84a6542d3ad61c6d6a
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-10-04 06:29:47 +0800

    Remove pkg install part of building testvm
    pkg should be bootstrapped from scratch
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-srcproj-openssl111-amd64-testvm/build.sh
+++ jobs/FreeBSD-srcproj-openssl111-amd64-testvm/build.sh
@@ -10,4 +10,71 @@ export WITH_DEBUG=1
 export WITH_DOC=1
 export WITH_TESTS=1
 
-sh -x freebsd-ci/scripts/build/build-test_image.sh
+SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
+
+set -ex
+
+if [ -z "${SVN_REVISION}" ]; then
+	echo "No subversion revision specified"
+	exit 1
+fi
+
+ARTIFACT_SERVER=${ARTIFACT_SERVER:-https://artifact.ci.freebsd.org}
+ARTIFACT_SUBDIR=snapshot/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+CONFIG_BASE=`dirname $0 | xargs realpath`/config
+OUTPUT_IMG_NAME=disk-test.img
+
+sudo rm -fr work
+mkdir -p work
+cd work
+
+DIST_PACKAGES="base kernel"
+if [ "${WITH_DOC}" = 1 ]; then
+	DIST_PACKAGES="${DIST_PACKAGES} doc"
+fi
+if [ "${WITH_TESTS}" = 1 ]; then
+	DIST_PACKAGES="${DIST_PACKAGES} tests"
+fi
+if [ "${WITH_DEBUG}" = 1 ]; then
+	DIST_PACKAGES="${DIST_PACKAGES} base-dbg kernel-dbg"
+fi
+if [ "${WITH_LIB32}" = 1 ]; then
+	DIST_PACKAGES="${DIST_PACKAGES} lib32"
+	if [ "${WITH_DEBUG}" = 1 ]; then
+		DIST_PACKAGES="${DIST_PACKAGES} lib32-dbg"
+	fi
+fi
+mkdir -p ufs
+for f in ${DIST_PACKAGES}
+do
+	fetch ${ARTIFACT_SERVER}/${ARTIFACT_SUBDIR}/${f}.txz
+	sudo tar Jxf ${f}.txz -C ufs
+done
+
+# copy default configs, existing files will be override
+sudo cp -Rf ${CONFIG_BASE}/testvm/override/ ufs/
+
+FROM=${CONFIG_BASE}/testvm/append/
+TO=ufs
+for i in `find ${FROM} -type f`
+do
+	f=${i#${FROM}}
+	mkdir -p ${TO}/`dirname $f`
+	cat ${FROM}$f | sudo tee -a ${TO}/$f > /dev/null
+done
+
+sudo makefs -d 6144 -t ffs -f 200000 -s 8g -o version=2,bsize=32768,fsize=4096 -Z ufs.img ufs
+mkimg -s gpt -f raw \
+	-b ufs/boot/pmbr \
+	-p freebsd-boot/bootfs:=ufs/boot/gptboot \
+	-p freebsd-swap/swapfs::1G \
+	-p freebsd-ufs/rootfs:=ufs.img \
+	-o ${OUTPUT_IMG_NAME}
+xz -0 ${OUTPUT_IMG_NAME}
+
+cd ${WORKSPACE}
+rm -fr artifact
+mkdir -p artifact/${ARTIFACT_SUBDIR}
+mv work/${OUTPUT_IMG_NAME}.xz artifact/${ARTIFACT_SUBDIR}
+
+echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

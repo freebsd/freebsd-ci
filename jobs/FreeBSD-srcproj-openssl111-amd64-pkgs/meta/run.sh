5cbefc27f317ca6feeca3d158213a211392d8852
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-10-07 06:06:15 +0800

    Add testing network configuration
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-srcproj-openssl111-amd64-pkgs/meta/run.sh
+++ jobs/FreeBSD-srcproj-openssl111-amd64-pkgs/meta/run.sh
@@ -2,6 +2,12 @@
 
 METADIR=/meta
 
+# config network
+# XXX: need to be parameterized
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+ifconfig vtnet0 8.8.178.219/27
+route add default 8.8.178.193
+
 zpool create tank /dev/ada2
 zfs set atime=off tank
 zfs set compression=lz4 tank

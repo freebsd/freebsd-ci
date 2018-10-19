8a4f29df71f2b278d37ab25302b4afaea438ba24
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-10-19 15:47:17 -0700

    Add mips and powerpc 12-STABLE jobs
    Sponsored by:	The FreeBSD Foundation

---
+++ jobs/FreeBSD-stable-12-powerpcspe-build/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+env \
+	JFLAG=${BUILDER_JFLAG} \
+	TARGET=powerpc \
+	TARGET_ARCH=powerpcspe \
+	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
+	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh

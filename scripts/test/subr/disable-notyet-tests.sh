2f09a00799dab0fe22baec6b26b8b934058b449f
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-10-24 19:12:55 +0800

    Temporarily disable frag6 tests before py27-scapy and py36-scapy can co-exist
    Sponsored by:	The FreeBSD Foundation

--- scripts/test/subr/disable-notyet-tests.sh
+++ scripts/test/subr/disable-notyet-tests.sh
@@ -6,3 +6,10 @@ if [ -f ${F} ]; then
 		-e 's/tap_test_program{name="dns_test", }/-- tap_test_program{name="dns_test", }/' \
 		${F}
 fi
+
+F=/usr/tests/sys/netinet6/Kyuafile
+if [ -f ${F} ]; then
+	sed -i .bak \
+		-e 's,include("frag6/Kyuafile"),-- include("frag6/Kyuafile"),' \
+		${F}
+fi

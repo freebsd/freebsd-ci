2bbcaec6210bb6e1ee6d2f06ebf1b9714b601965
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-12-23 23:02:05 +0800

    Explicitly continue running the script when ltp exits with non-zero code
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-amd64-test_ltp/meta/run.sh
+++ jobs/FreeBSD-head-amd64-test_ltp/meta/run.sh
@@ -23,7 +23,9 @@ utstest_unshare_3 utstest_unshare_3
 utstest_unshare_4 utstest_unshare_4
 END
 
+set +e
 yes | limits -n 1024 chroot /compat/linux /opt/ltp/runltp -Q -S /ltp-skipfile.conf -pl /ltp-results.log
-
 echo $? > ${METADIR}/runltp.error
+set -e
+
 mv -v /compat/linux/ltp-results.log ${METADIR}

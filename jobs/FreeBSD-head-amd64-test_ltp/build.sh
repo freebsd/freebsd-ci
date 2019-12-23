f763506371e05c39386a9cca868423cafe707ea6
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-12-23 18:53:51 +0000

    We have ltp2junit.sh now so don't have to exit with ltp's exit code
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-amd64-test_ltp/build.sh
+++ jobs/FreeBSD-head-amd64-test_ltp/build.sh
@@ -64,5 +64,3 @@ sudo /usr/sbin/bhyvectl --vm=${TEST_VM_NAME} --destroy
 sh -ex ${TEST_BASE}/extract-meta.sh
 sh -ex ${TEST_BASE}/ltp2junit.sh ${METAOUTDIR}/ltp-results.log test-report.xml
 rm -f ${IMG_NAME}
-
-exit `cat ${METAOUTDIR}/runltp.error`

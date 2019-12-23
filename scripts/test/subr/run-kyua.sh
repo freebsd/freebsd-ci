41ced7d46fe7a533517299e7f8093e959f8f1c7c
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-12-24 03:48:07 +0800

    Whitespace clean
    Sponsored by:	The FreeBSD Foundation

--- scripts/test/subr/run-kyua.sh
+++ scripts/test/subr/run-kyua.sh
@@ -7,12 +7,12 @@ echo "--------------------------------------------------------------"
 
 cd /usr/tests
 set +e
-/usr/local/bin/kyua test                                                       
+/usr/local/bin/kyua test
 rc=$?
 set -e
 if [ ${rc} -ne 0 ] && [ ${rc} -ne 1 ]; then
 	exit ${rc}
 fi
 /usr/local/bin/kyua report --verbose --results-filter passed,skipped,xfail,broken,failed --output test-report.txt
-/usr/local/bin/kyua report-junit --output=test-report.xml                      
+/usr/local/bin/kyua report-junit --output=test-report.xml
 mv test-report.* /meta

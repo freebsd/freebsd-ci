57d80e0b0608f3cb6d329816bda4d7836f338400
Li-Wen Hsu <lwhsu@lwhsu.org>
2020-01-13 21:00:45 +0800

    Fix ARTIFACT_DEST and reduce diff with shared script
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-riscv64-testvm/build.sh
+++ jobs/FreeBSD-head-riscv64-testvm/build.sh
@@ -2,26 +2,28 @@
 
 SSL_CA_CERT_FILE=/usr/local/share/certs/ca-root-nss.crt
 
-JOB_BASE=${WORKSPACE}/freebsd-ci/jobs/${JOB_NAME}
+set -ex
 
-TARGET=riscv
-TARGET_ARCH=riscv64
+JOB_BASE=${WORKSPACE}/freebsd-ci/jobs/${JOB_NAME}
 
 if [ -z "${SVN_REVISION}" ]; then
 	echo "No subversion revision specified"
 	exit 1
 fi
 
-ARTIFACT_SUBDIR=${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
-ARTIFACT_DEST=${WORKSPACE}/artifact/${ARTIFACT_SUBDIR}
-rm -fr ${WORKSPACE}/artifact
-mkdir -p ${ARTIFACT_DEST}
+TARGET=riscv
+TARGET_ARCH=riscv64
+
+ARTIFACT_SERVER=${ARTIFACT_SERVER:-https://artifact.ci.freebsd.org}
+ARTIFACT_SUBDIR=snapshot/${FBSD_BRANCH}/r${SVN_REVISION}/${TARGET}/${TARGET_ARCH}
+ARTIFACT_DEST=artifact/${ARTIFACT_SUBDIR}
 
 cd ${WORKSPACE}
+
 sudo rm -fr base || true
 sudo chflags -R noschg base || true
 sudo rm -fr base
-fetch https://artifact.ci.freebsd.org/snapshot/${ARTIFACT_SUBDIR}/base.txz
+fetch ${ARTIFACT_SERVER}/${ARTIFACT_SUBDIR}/base.txz
 mkdir base
 sudo tar Jxf base.txz -C base
 
@@ -33,7 +35,9 @@ sudo dd if=/dev/random of=base/boot/entropy bs=4k count=1
 sudo rm -f riscv.img riscv.img.xz
 sudo makefs -D -f 200000 -s 2g -o version=2 -Z riscv.img base
 xz -0 riscv.img
+
+rm -fr artifact
+mkdir -p ${ARTIFACT_DEST}
 mv riscv.img.xz ${ARTIFACT_DEST}
 
-cd ${WORKSPACE}
 echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

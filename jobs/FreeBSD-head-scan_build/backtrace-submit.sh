6df91429b1f515a7764b79e0c3aac7431f15267c
Li-Wen Hsu <lwhsu@lwhsu.org>
2020-03-21 03:42:08 +0800

    Submit Clang scan build result to FreeBSD's Backtrace instance
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-scan_build/backtrace-submit.sh
+++ jobs/FreeBSD-head-scan_build/backtrace-submit.sh
@@ -6,54 +6,65 @@ INPUT="$1"
 BUILD="$2"
 URL="$3"
 
-if test -z "$URL"; then
-	URL="https://freebsd.sp.backtrace.io:6098/post?format=json&token=<token>"
+TOKEN=${BACKTRACE_TOKEN}
+if [ -z "${TOKEN}" ]; then
+	echo "BACKTRACE_TOKEN is not defined"
+	exit 1
+fi
+
+if test -z "${URL}"; then
+	URL="https://freebsd.sp.backtrace.io:6098/post?format=json&token=${TOKEN}"
+fi
+
+if test -z "${BUILD}"; then
+	BUILD=tmp.backtrace
 fi
 
 # The default number of reports to include in every chunk.
 CHUNK_SIZE=200
 
-mkdir -p "$BUILD/archives"
+mkdir -p "${BUILD}/archives"
 
 SIZE_LIMIT="1000000c"
 
-REPORTS_COUNT=`find $1 -name '*.html' -size -${SIZE_LIMIT} | wc -l`
-echo "Found $REPORTS_COUNT reports meeting size criteria..."
+REPORTS_COUNT=`find ${INPUT} -name '*.html' -size -${SIZE_LIMIT} | wc -l`
+echo "Found ${REPORTS_COUNT} reports meeting size criteria..."
 
 # We limit individual report size for now, this will be removed in the future.
-find $1 -name '*.html' -size -${SIZE_LIMIT} | split -l $CHUNK_SIZE - $BUILD/chunk.
+find ${INPUT} -name '*.html' -size -${SIZE_LIMIT} | split -l ${CHUNK_SIZE} - ${BUILD}/chunk.
 
-CHUNK_COUNT=`find $BUILD -maxdepth 1 -type f -name "chunk.*" | wc -l`
+CHUNK_COUNT=`find ${BUILD} -maxdepth 1 -type f -name "chunk.*" | wc -l`
 
-echo "Generating $CHUNK_COUNT chunks..."
+echo "Generating ${CHUNK_COUNT} chunks..."
 
 counter=1
-for i in `find $BUILD/ -maxdepth 1 -type f -name "chunk.*"`; do
+for i in `find ${BUILD}/ -maxdepth 1 -type f -name "chunk.*"`; do
 	CHUNK=`basename $i`
 
 	echo "    + $counter [${CHUNK}]"
 	counter=`expr $counter + 1`
 
-	tar cTfz "$i" "${BUILD}/archives/${CHUNK}.tar.gz" 
+	tar cTfz "$i" "${BUILD}/archives/${CHUNK}.tar.gz"
 done
 
 rm -f ${BUILD}/chunk.*
 
-echo "Submitting $CHUNK_COUNT chunks..."
+echo "Submitting ${CHUNK_COUNT} chunks..."
 
 counter=1
 
-#QUERY_STRING="author=${CHANGE_AUTHOR}&build_id=${BUILD_ID}&build_number=${BUILD_NUMBER}&job_name=${JOB_NAME}&build_url=${BUILD_URL}&svn_revision=${SVN_REVISION}"
+QUERY_STRING="author=${CHANGE_AUTHOR}&build_id=${BUILD_ID}&build_number=${BUILD_NUMBER}&job_name=${JOB_NAME}&build_url=${BUILD_URL}&svn_revision=${SVN_REVISION}"
 
-for i in `find $BUILD/archives -type f -name '*.tar.gz'`; do
+for i in `find ${BUILD}/archives -type f -name '*.tar.gz'`; do
 	CHUNK=`basename $i`
 
-	echo "    + $counter [${CHUNK}]"
-	counter=`expr $counter + 1`
+	echo "    + ${counter} [${CHUNK}]"
+	counter=`expr ${counter} + 1`
 
-	curl -v -X POST https://sca.backtrace.io/api/sca/submit/clang-analyzer?$QUERY_STRING	\
+	curl -v -X POST https://sca.backtrace.io/api/sca/submit/clang-analyzer?${QUERY_STRING}	\
 	  -H 'Content-Type: multipart/form-data'						\
 	  -F report="@${i}" 									\
 	  -F "submitUrl=${URL}"
 done
 
+rm -rf ${BUILD}

5e8704617365488891576e27d4325e0474bc90de
Li-Wen Hsu <lwhsu@lwhsu.org>
2020-03-24 00:30:47 +0800

    Switch to Clang and LLD as the toolchain for MIPS
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-head-mips-build/build.sh
+++ jobs/FreeBSD-head-mips-build/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-SRCCONF=${WORKSPACE}/`dirname $0`/src.conf
-
 env \
 	JFLAG=${BUILDER_JFLAG} \
-	SRCCONF=${SRCCONF} \
 	TARGET=mips \
 	TARGET_ARCH=mips \
-	CROSS_TOOLCHAIN=mips-gcc6 \
+	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
 	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh
--- jobs/FreeBSD-head-mips64-build/build.sh
+++ jobs/FreeBSD-head-mips64-build/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-SRCCONF=${WORKSPACE}/`dirname $0`/src.conf
-
 env \
 	JFLAG=${BUILDER_JFLAG} \
-	SRCCONF=${SRCCONF} \
 	TARGET=mips \
 	TARGET_ARCH=mips64 \
-	CROSS_TOOLCHAIN=mips64-gcc6 \
+	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
 	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh

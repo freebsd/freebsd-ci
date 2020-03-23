7458677b2fa7e76219d686cccd3595b47ec3d02d
Edward Tomasz Napierala <trasz@FreeBSD.org>
2020-03-23 19:03:24 +0000

    Add FreeBSD-head-armv6-test, copied almost verbatim from FreeBSD-head-amd64-test.

---
+++ jobs/FreeBSD-head-armv6-test/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export TARGET=arm
+export TARGET_ARCH=armv6
+
+export USE_TEST_SUBR="
+disable-dtrace-tests.sh
+disable-zfs-tests.sh
+disable-notyet-tests.sh
+run-kyua.sh
+"
+
+sh -x freebsd-ci/scripts/test/run-tests.sh

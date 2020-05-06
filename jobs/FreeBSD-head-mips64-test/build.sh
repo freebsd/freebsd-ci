351e1ea451537cae69cd582c0c459dc170a09332
Edward Tomasz Napierala <trasz@FreeBSD.org>
2020-05-06 15:47:59 +0100

    Fix memory size.

--- jobs/FreeBSD-head-mips64-test/build.sh
+++ jobs/FreeBSD-head-mips64-test/build.sh
@@ -9,6 +9,9 @@ export QEMU_MACHINE="malta"
 # XXX: The MALTA64 config doesn't support neither AHCI nor virtio.
 export QEMU_EXTRA_PARAM="-kernel kernel -drive if=none,file=disk-test.img,format=raw,id=hd2 -device ide-hd,bus=ide.0,drive=hd2 -drive if=none,file=meta.tar,format=raw,id=hd3 -device ide-hd,bus=ide.1,drive=hd3"
 
+# The "malta" machine doesn't support more than 2G.
+export VM_MEM_SIZE="2g"
+
 # XXX: Temporary, to compare performance results.
 export VM_CPU_COUNT=1
 

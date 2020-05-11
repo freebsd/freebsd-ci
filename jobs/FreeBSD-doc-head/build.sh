ce204a6a90a93331a83c1247274ba57a7cb4ea59
Li-Wen Hsu <lwhsu@lwhsu.org>
2020-05-12 05:39:20 +0800

    Disable fetching ports INDEX file temporily
    This operation sometimes fails and need more time to debug
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-doc-head/build.sh
+++ jobs/FreeBSD-doc-head/build.sh
@@ -4,6 +4,6 @@ cd doc
 make
 
 cd en_US.ISO8859-1/htdocs
-make -j ${BUILDER_JFLAG}
+make -j ${BUILDER_JFLAG} PINDEX_OVERRIDE=/dev/null
 
 echo "SVN_REVISION=${SVN_REVISION}" > ${WORKSPACE}/trigger.property

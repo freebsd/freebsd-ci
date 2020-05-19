986db4310e2b3afe274a36fb2250de4f6c49565d
Edward Tomasz Napierala <trasz@FreeBSD.org>
2020-05-19 19:15:36 +0100

    Add cap-hpt-max-page-size=16M to FreeBSD-head-powerpc64-test.
    This should improve performance, kudos to luporl@ for the hint.

--- jobs/FreeBSD-head-powerpc64-test/build.sh
+++ jobs/FreeBSD-head-powerpc64-test/build.sh
@@ -4,7 +4,9 @@ export TARGET=powerpc
 export TARGET_ARCH=powerpc64
 export USE_QEMU=1
 export QEMU_ARCH="ppc64"
-export QEMU_MACHINE="pseries"
+# The cap-hpt-max-page-size is to get rid of the "mmu_phyp: Support for hugepages not found"
+# warning on boot.  It's expected to improve performance.
+export QEMU_MACHINE="pseries,cap-hpt-max-page-size=16M"
 # XXX: Note the reversed order; otherwise the metadisk would end up as vtbd0
 export QEMU_DEVICES="-device virtio-blk,drive=hd1 -device virtio-blk,drive=hd0"
 export QEMU_EXTRA_PARAM="-vga none"

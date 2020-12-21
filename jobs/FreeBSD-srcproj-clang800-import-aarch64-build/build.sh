786609de8ce8a0f6782bd5feae56e90be21d6e3c
Brandon Bergren <bdragon@FreeBSD.org>
2020-12-21 17:32:12 -0600

    JJB cleanup: Delete old clang800-import jobs.

--- jobs/FreeBSD-srcproj-clang800-import-aarch64-build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export JFLAG=${BUILDER_JFLAG}
-export SRCCONF=${WORKSPACE}/`dirname $0`/src.conf
-export TARGET=arm64
-export TARGET_ARCH=aarch64
-
-sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh

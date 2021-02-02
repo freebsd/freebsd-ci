4ee1b26baf81432116be8e8ce9499f67f0ba43db
Brandon Bergren <bdragon@FreeBSD.org>
2021-02-02 09:21:19 -0600

    Update freebsd-doc

--- jobs/FreeBSD-doc-main/build.sh
+++ jobs/FreeBSD-doc-main/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cd doc
-make
+make -j ${BUILDER_JFLAG}
 
-cd en_US.ISO8859-1/htdocs
-make -j ${BUILDER_JFLAG} PINDEX_OVERRIDE=/dev/null
+#cd en_US.ISO8859-1/htdocs
+#make -j ${BUILDER_JFLAG} PINDEX_OVERRIDE=/dev/null
 
 echo "GIT_COMMIT=${GIT_COMMIT}" > ${WORKSPACE}/trigger.property

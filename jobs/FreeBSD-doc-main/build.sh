e2d47875680793587dda924440453109aadb236d
Li-Wen Hsu <lwhsu@lwhsu.org>
2021-02-04 16:53:42 +0800

    Update FreeBSD-doc-main
    - Use latest -RELEASE as build platform.
    - `make -j2` for two subdir is enough
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-doc-main/build.sh
+++ jobs/FreeBSD-doc-main/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 cd doc
-make -j ${BUILDER_JFLAG}
-
-#cd en_US.ISO8859-1/htdocs
-#make -j ${BUILDER_JFLAG} PINDEX_OVERRIDE=/dev/null
+make -j 2
 
 echo "GIT_COMMIT=${GIT_COMMIT}" > ${WORKSPACE}/trigger.property

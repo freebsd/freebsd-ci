4fe9ee04b70620e8a59ec142ec9ea9fe181e21bf
Li-Wen Hsu <lwhsu@lwhsu.org>
2022-03-31 04:54:21 +0800

    Remove main-mips* jobs
    mips* support has been removed from main.
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-main-mips64-test/meta/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-METADIR=/meta
-sh -ex ${METADIR}/disable-disks-tests.sh
-sh -ex ${METADIR}/disable-dtrace-tests.sh
-sh -ex ${METADIR}/disable-zfs-tests.sh
-sh -ex ${METADIR}/disable-notyet-tests.sh
-sh -ex ${METADIR}/run-kyua.sh

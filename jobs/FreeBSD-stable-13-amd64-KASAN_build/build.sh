3cc390792646bbab7ff99a47c9c6ce3a3b26b976
Li-Wen Hsu <lwhsu@lwhsu.org>
2023-03-24 23:46:06 +0800

    Use make.conf to specify KERNCONF
    Thus the other script can catch up the name of customized kernel.
    
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-stable-13-amd64-KASAN_build/build.sh
+++ jobs/FreeBSD-stable-13-amd64-KASAN_build/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+JOBDIR=${WORKSPACE}/`dirname $0`
+
 env \
 	JFLAG=${BUILDER_JFLAG} \
 	TARGET=amd64 \
 	TARGET_ARCH=amd64 \
-	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
+	MAKECONF=${JOBDIR}/make.conf \
+	SRCCONF=${JOBDIR}/src.conf \
 	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh

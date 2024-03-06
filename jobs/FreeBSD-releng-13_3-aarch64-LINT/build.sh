b289e82eccafa109f2cfa893e13907c35840c867
Li-Wen Hsu <lwhsu@FreeBSD.org>
2024-03-06 19:41:06 +0800

    13.3-RELEASE is out of the door
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-releng-13_3-aarch64-LINT/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=arm64 \
-	TARGET_ARCH=aarch64 \
-	sh -x freebsd-ci/scripts/build/build-kernel-LINT-head.sh
--- jobs/FreeBSD-releng-13_3-armv7-LINT/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=arm \
-	TARGET_ARCH=armv7 \
-	sh -x freebsd-ci/scripts/build/build-kernel-LINT-head.sh

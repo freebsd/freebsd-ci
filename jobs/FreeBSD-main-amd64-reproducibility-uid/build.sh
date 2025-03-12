7d048b84cb5c3c0c7ac03b6ca6011791fc3c9b12
Muhammad Moinur Rahman <moin@bofh.im>
2025-03-12 13:18:49 +0000

    Decouple uid test from build-reproducible.sh script

--- jobs/FreeBSD-main-amd64-reproducibility-uid/build.sh
+++ jobs/FreeBSD-main-amd64-reproducibility-uid/build.sh
@@ -1,9 +1,44 @@
 #!/bin/sh
 
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=amd64 \
-	TARGET_ARCH=amd64 \
-	SRCCONF=${WORKSPACE}/`dirname $0`/src.conf \
-	TESTTYPE=uid \
-	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-reproducible.sh
+set -ex
+
+export TESTTYPE=uid
+. ${WORKSPACE}/freebsd-ci/scripts/build/build-reproducible-pre.sh
+export TARGET=amd64
+export TARGET_ARCH=amd64
+
+echo $SOURCE_DATE_EPOCH
+echo $SOURCE_DATE_EPOCH_BASE
+cd /usr/src
+sudo -u root -E make -j ${JFLAG} \
+	buildworld \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	${CROSS_TOOLCHAIN_PARAM} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo -u root -E make -j ${JFLAG} -DNO_CLEAN -DNO_ROOT \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	${CROSS_TOOLCHAIN_PARAM} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+export MAKEOBJDIRPREFIX=${WORKSPACE}/objtest
+rm -fr ${MAKEOBJDIRPREFIX}
+sudo -u nobody -E make -j ${JFLAG} \
+	buildworld \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	${CROSS_TOOLCHAIN_PARAM} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+sudo -u nobody -E make -j ${JFLAG} \
+	buildkernel \
+	TARGET=${TARGET} \
+	TARGET_ARCH=${TARGET_ARCH} \
+	${CROSS_TOOLCHAIN_PARAM} \
+	__MAKE_CONF=${MAKECONF} \
+	SRCCONF=${SRCCONF}
+
+. ${WORKSPACE}/freebsd-ci/scripts/build/build-reproducible.sh

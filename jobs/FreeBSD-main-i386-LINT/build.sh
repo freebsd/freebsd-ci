9805225feeaef652134f0db9ede148bbc4fb9062
Li-Wen Hsu <lwhsu@lwhsu.org>
2025-07-26 21:17:24 +0800

    Remove 32-bit architecture jobs for the main branch
    Sponsored by:	The FreeBSD Foundation

--- jobs/FreeBSD-main-i386-LINT/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=i386 \
-	TARGET_ARCH=i386 \
-	sh -x freebsd-ci/scripts/build/build-kernel-LINT-head.sh
--- jobs/FreeBSD-main-i386-build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-env \
-	JFLAG=${BUILDER_JFLAG} \
-	TARGET=i386 \
-	TARGET_ARCH=i386 \
-	sh -x ${WORKSPACE}/freebsd-ci/scripts/build/build-world-kernel-head.sh
